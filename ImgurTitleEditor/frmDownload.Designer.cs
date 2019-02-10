22fa5731ae26c062c02ff83219e36c880133cdeb
Kevin Gut <kevin.gut@bitm.sg>
2019-02-10 14:03:29 +0100

    Remove Unnecessary Dialog Result

--- ImgurTitleEditor/frmDownload.Designer.cs
+++ ImgurTitleEditor/frmDownload.Designer.cs
@@ -44,7 +44,6 @@
             // btnCancel
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.Location = new System.Drawing.Point(417, 21);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
