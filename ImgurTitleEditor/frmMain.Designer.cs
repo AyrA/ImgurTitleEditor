5f0f7122e7b9a54eab5cc7f477dbd69708b5e51c
Kevin Gut <kevin.gut@bitm.sg>
2019-02-10 01:09:21 +0100

    Image List

--- ImgurTitleEditor/frmMain.Designer.cs
+++ ImgurTitleEditor/frmMain.Designer.cs
@@ -34,6 +34,10 @@
             this.authorizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.buildCacheToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.allImagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.withTitleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.withoutTitleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,7 +55,8 @@
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
+            this.fileToolStripMenuItem,
+            this.listToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(592, 24);
@@ -89,6 +94,37 @@
             this.exitToolStripMenuItem.Text = "&Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
+            // listToolStripMenuItem
+            // 
+            this.listToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.allImagesToolStripMenuItem,
+            this.withTitleToolStripMenuItem,
+            this.withoutTitleToolStripMenuItem});
+            this.listToolStripMenuItem.Name = "listToolStripMenuItem";
+            this.listToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.listToolStripMenuItem.Text = "&List";
+            // 
+            // allImagesToolStripMenuItem
+            // 
+            this.allImagesToolStripMenuItem.Name = "allImagesToolStripMenuItem";
+            this.allImagesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.allImagesToolStripMenuItem.Text = "&All Images";
+            this.allImagesToolStripMenuItem.Click += new System.EventHandler(this.allImagesToolStripMenuItem_Click);
+            // 
+            // withTitleToolStripMenuItem
+            // 
+            this.withTitleToolStripMenuItem.Name = "withTitleToolStripMenuItem";
+            this.withTitleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.withTitleToolStripMenuItem.Text = "&With Title";
+            this.withTitleToolStripMenuItem.Click += new System.EventHandler(this.withTitleToolStripMenuItem_Click);
+            // 
+            // withoutTitleToolStripMenuItem
+            // 
+            this.withoutTitleToolStripMenuItem.Name = "withoutTitleToolStripMenuItem";
+            this.withoutTitleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.withoutTitleToolStripMenuItem.Text = "With&out Title";
+            this.withoutTitleToolStripMenuItem.Click += new System.EventHandler(this.withoutTitleToolStripMenuItem_Click);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -113,6 +149,10 @@
         private System.Windows.Forms.ToolStripMenuItem authorizeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem buildCacheToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem allImagesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem withTitleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem withoutTitleToolStripMenuItem;
     }
 }
 
--- ImgurTitleEditor/frmMain.cs
+++ ImgurTitleEditor/frmMain.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ImgurTitleEditor
 {
     public partial class frmMain : Form
     {
+        private enum ImageFilter
+        {
+            All,
+            WithTitle,
+            WithoutTitle
+        }
+
         private Imgur I;
         private Settings S;
 
@@ -12,6 +24,8 @@ namespace ImgurTitleEditor
         {
             this.S = S;
             InitializeComponent();
+            //Program.Main already tries to authorize.
+            //If the date is still in the past, the user has to do so manually.
             if (S.Token.Expires < DateTime.UtcNow)
             {
                 using (var f = new frmAuth(S))
@@ -23,11 +37,11 @@ namespace ImgurTitleEditor
                     else
                     {
                         MessageBox.Show("Could not authorize this application", "No Authorization", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        Application.Exit();
                     }
                 }
             }
             I = new Imgur(S);
+            ShowImages(ImageFilter.All);
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -42,6 +56,7 @@ namespace ImgurTitleEditor
                 using (var fCache = new frmCacheBuilder(S))
                 {
                     fCache.ShowDialog();
+                    ShowImages(ImageFilter.All);
                 }
             }
         }
@@ -56,11 +71,80 @@ namespace ImgurTitleEditor
                     {
                         Cache.Images = null;
                         Cache.ClearThumbnails();
-                        ShowImages();
+                        ShowImages(ImageFilter.All);
                     }
                 }
             }
-            //
+        }
+
+        private void ShowImages(ImageFilter Filter)
+        {
+            //const int BATCHSIZE = 100;
+            lvImages.Items.Clear();
+            var T = new Thread(delegate ()
+            {
+                var Items = new List<ListViewItem>();
+                var IL = new ImageList()
+                {
+                    ImageSize = new Size(128, 128),
+                    ColorDepth = ColorDepth.Depth16Bit
+                };
+                foreach (var I in Cache.Images)
+                {
+                    if (
+                        Filter == ImageFilter.All ||
+                        (Filter == ImageFilter.WithTitle && !string.IsNullOrWhiteSpace(I.title)) ||
+                        (Filter == ImageFilter.WithoutTitle && string.IsNullOrWhiteSpace(I.title)))
+                    {
+                        using (var MS = new MemoryStream(Cache.GetThumbnail(I)))
+                        {
+                            IL.Images.Add(Image.FromStream(MS));
+                        }
+                        var Item = new ListViewItem(I.title == null ? string.Empty : I.title);
+                        Item.ImageIndex = IL.Images.Count - 1;
+                        Item.Tag = I;
+                        Item.ToolTipText = $"[{I.name}] {I.description}";
+                        Items.Add(Item);
+                    }
+                }
+                Invoke((MethodInvoker)delegate
+                {
+                    lvImages.Items.Clear();
+                    if (lvImages.LargeImageList != null)
+                    {
+                        lvImages.LargeImageList.Dispose();
+                    }
+                    lvImages.LargeImageList = IL;
+                    lvImages.Items.AddRange(Items.ToArray());
+                });
+
+                /*
+                for (var i = 0; i < Items.Count; i += BATCHSIZE)
+                {
+                    Invoke((MethodInvoker)delegate
+                    {
+                        lvImages.Items.AddRange(Items.Skip(i).Take(BATCHSIZE).ToArray());
+                    });
+                }
+                //*/
+            });
+            T.IsBackground = true;
+            T.Start();
+        }
+
+        private void allImagesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowImages(ImageFilter.All);
+        }
+
+        private void withTitleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowImages(ImageFilter.WithTitle);
+        }
+
+        private void withoutTitleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowImages(ImageFilter.WithoutTitle);
         }
     }
 }
