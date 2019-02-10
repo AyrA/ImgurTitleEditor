f9e519f97b8a58bd7e5ac3ddc2fec38092069bc0
Kevin Gut <kevin.gut@bitm.sg>
2019-02-10 13:50:56 +0100

    Download Form Design

--- ImgurTitleEditor/frmDownload.Designer.cs
+++ ImgurTitleEditor/frmDownload.Designer.cs
@@ -28,11 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbStatus = new System.Windows.Forms.ProgressBar();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbStatus.Location = new System.Drawing.Point(12, 21);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(395, 23);
+            this.pbStatus.TabIndex = 0;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(417, 21);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmDownload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "frmDownload";
+            this.ClientSize = new System.Drawing.Size(504, 65);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.pbStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmDownload";
+            this.Text = "Image Download";
+            this.Shown += new System.EventHandler(this.frmDownload_Shown);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar pbStatus;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
--- ImgurTitleEditor/frmDownload.cs
+++ ImgurTitleEditor/frmDownload.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ImgurTitleEditor
 {
     public partial class frmDownload : Form
     {
-        public frmDownload()
+        private bool Exit = false;
+        private List<ImgurImage> Images;
+        private Settings S;
+        private string Path;
+
+        public frmDownload(Settings S, IEnumerable<ImgurImage> Images, string Path)
         {
+            this.S = S;
+            this.Images = Images.ToList();
+            this.Path = Path;
             InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Exit = true;
+            Close();
+        }
+
+        private void frmDownload_Shown(object sender, EventArgs e)
+        {
+
         }
     }
 }
