835897043459952ea64d28ad1b65683f120635eb
Kevin Gut <kevin.gut@bitm.sg>
2019-02-10 13:59:30 +0100

    Download logic

--- ImgurTitleEditor/frmDownload.cs
+++ ImgurTitleEditor/frmDownload.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ImgurTitleEditor
@@ -22,13 +23,48 @@ namespace ImgurTitleEditor
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
+            btnCancel.Text = "Cancelling...";
+            btnCancel.Enabled = false;
             Exit = true;
-            Close();
+            //Close();
         }
 
         private void frmDownload_Shown(object sender, EventArgs e)
         {
+            pbStatus.Maximum = Images.Count;
+            Thread T = new Thread(delegate ()
+            {
+                while (Images.Count > 0 && !Exit)
+                {
+                    var I = Images[0];
+                    Images.RemoveAt(0);
+                    //Just cache if path is not defined
+                    if (string.IsNullOrEmpty(Path))
+                    {
+                        Cache.GetImage(I);
+                    }
+                    else
+                    {
+                        var FileName = System.IO.Path.Combine(Path, I.GetImageUrl().Segments.Last());
+                        if (FileName.StartsWith(Path))
+                        {
+                            System.IO.File.WriteAllBytes(FileName, Cache.GetImage(I));
+                        }
+                    }
+#if DEBUG
+                    System.Diagnostics.Debug.WriteLine($"URL ({I.GetImageUrl()}) tried to get out of {Path}");
+#endif
+                    Invoke((MethodInvoker)delegate { ++pbStatus.Value; });
+                }
+                Invoke((MethodInvoker)Done);
+            });
+            T.IsBackground = true;
+            T.Start();
+        }
 
+        private void Done()
+        {
+            Close();
         }
     }
 }
