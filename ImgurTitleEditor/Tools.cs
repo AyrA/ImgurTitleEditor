adba9dba147bd8f72f8afa4c980cbf5831428bdf
Kevin Gut <kevin.gut@bitm.sg>
2019-02-14 14:29:50 +0100

    Comments for Tools.cs

--- ImgurTitleEditor/Tools.cs
+++ ImgurTitleEditor/Tools.cs
@@ -9,8 +9,18 @@ using System.Xml.Serialization;
 
 namespace ImgurTitleEditor
 {
+    /// <summary>
+    /// Contains various utility functions
+    /// </summary>
     public static class Tools
     {
+        /// <summary>
+        /// Deserializes a JSON string into an object
+        /// </summary>
+        /// <typeparam name="T">Target object type</typeparam>
+        /// <param name="Source">JSON data</param>
+        /// <param name="Default">Default to return on error</param>
+        /// <returns>Deserialized object on success, <paramref name="Default"/> on error</returns>
         public static T FromJson<T>(this string Source, T Default = default(T))
         {
 #if DEBUG
@@ -27,11 +37,22 @@ namespace ImgurTitleEditor
             }
         }
 
+        /// <summary>
+        /// Serializes almost any object into a JSON string
+        /// </summary>
+        /// <param name="o">Object to serialize</param>
+        /// <returns>JSON string</returns>
         public static string ToJson(this object o)
         {
             return o == null ? "null" : JsonConvert.SerializeObject(o);
         }
 
+        /// <summary>
+        /// Serializes an object into an XML string
+        /// </summary>
+        /// <param name="o">Object to serialize</param>
+        /// <returns>XML string</returns>
+        /// <remarks>Object and all properties (recursively) must have the <see cref="SerializableAttribute"/></remarks>
         public static string ToXml(this object o)
         {
             if (o == null)
@@ -46,6 +67,13 @@ namespace ImgurTitleEditor
             }
         }
 
+        /// <summary>
+        /// Deserializes an XML string into an object
+        /// </summary>
+        /// <typeparam name="T">Target object type</typeparam>
+        /// <param name="s">XML string</param>
+        /// <returns></returns>
+        /// <remarks>Object and all properties (recursively) must have the <see cref="SerializableAttribute"/></remarks>
         public static T FromXml<T>(this string s)
         {
             if (string.IsNullOrWhiteSpace(s))
@@ -59,6 +87,12 @@ namespace ImgurTitleEditor
             }
         }
 
+        /// <summary>
+        /// Saves the given settings to a file
+        /// </summary>
+        /// <param name="S">Settings</param>
+        /// <param name="FileName">File path/name</param>
+        /// <returns>Serialized settings data</returns>
         public static string SaveSettings(Settings S, string FileName)
         {
             var Ret = S.Save();
@@ -66,23 +100,46 @@ namespace ImgurTitleEditor
             return Ret;
         }
 
+        /// <summary>
+        /// Loads settings from a file
+        /// </summary>
+        /// <param name="FileName">File path/name</param>
+        /// <returns>Deserialized <see cref="Settings"/> object</returns>
         public static Settings LoadSettings(string FileName)
         {
             return Settings.Load(File.ReadAllText(FileName));
         }
 
+        /// <summary>
+        /// Tries to convert a string to a <see cref="long"/>
+        /// </summary>
+        /// <param name="Source">String to convert</param>
+        /// <param name="Default">Value to return on error</param>
+        /// <returns>Int64 or default value</returns>
         public static long LongOrDefault(string Source, long Default = long.MinValue)
         {
             long L;
             return long.TryParse(Source, out L) ? L : Default;
         }
 
+        /// <summary>
+        /// Tries to convert a string to a <see cref="int"/>
+        /// </summary>
+        /// <param name="Source">String to convert</param>
+        /// <param name="Default">Value to return on error</param>
+        /// <returns>Int32 or default value</returns>
         public static long IntOrDefault(string Source, int Default = int.MinValue)
         {
             int I;
             return int.TryParse(Source, out I) ? I : Default;
         }
 
+        /// <summary>
+        /// Parses an URL fragment or query string into a simple dictionary
+        /// </summary>
+        /// <param name="U">URL</param>
+        /// <param name="UseQueryString">Use query string (?...) instead of fragment (#...)</param>
+        /// <returns>URL parameter dictionary</returns>
         public static Dictionary<string, string> ParseFragment(Uri U, bool UseQueryString)
         {
             var F = UseQueryString ? U.Query : U.Fragment;
