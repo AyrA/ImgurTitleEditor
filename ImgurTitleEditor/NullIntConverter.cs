496a7a209984d176355e4a8df70c49141053f769
AyrA <admin@ayra.ch>
2020-04-24 01:56:22 +0200

    Add comments to JSON Nullable integer converter

--- ImgurTitleEditor/NullIntConverter.cs
+++ ImgurTitleEditor/NullIntConverter.cs
@@ -3,13 +3,29 @@ using System;
 
 namespace ImgurTitleEditor
 {
+    /// <summary>
+    /// Converts nullable integers
+    /// </summary>
     public class NullIntConverter : JsonConverter
     {
+        /// <summary>
+        /// Checks if a type is processable by this class
+        /// </summary>
+        /// <param name="objectType">Object type</param>
+        /// <returns><see cref="true"/>, if processable</returns>
         public override bool CanConvert(Type objectType)
         {
             return typeof(int) == objectType;
         }
 
+        /// <summary>
+        /// Reads an optional integer from the JSON
+        /// </summary>
+        /// <param name="reader">JSON reader</param>
+        /// <param name="objectType">Object type</param>
+        /// <param name="existingValue">Existing value</param>
+        /// <param name="serializer">JSON serializer</param>
+        /// <returns>Read number, or zero if <see cref="null"/></returns>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             if (reader.Value == null)
@@ -19,6 +35,12 @@ namespace ImgurTitleEditor
             return int.Parse(reader.Value.ToString());
         }
 
+        /// <summary>
+        /// Writes a value to the JSON
+        /// </summary>
+        /// <param name="writer">JSON writer</param>
+        /// <param name="value">Value to write</param>
+        /// <param name="serializer">JSON serializer</param>
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             writer.WriteValue(value);
