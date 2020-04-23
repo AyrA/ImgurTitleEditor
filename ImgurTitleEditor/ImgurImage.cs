9069f260016af64567ff86fb15004acb25a6ca33
AyrA <admin@ayra.ch>
2020-04-24 01:53:22 +0200

    Update image class

--- ImgurTitleEditor/ImgurImage.cs
+++ ImgurTitleEditor/ImgurImage.cs
@@ -46,109 +46,110 @@ namespace ImgurTitleEditor
     /// Imgur image
     /// </summary>
     [Serializable]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Imgur API")]
     public class ImgurImage
     {
         /// <summary>
         /// Image id
         /// </summary>
-        public string id;
+        public string id { get; set; }
         /// <summary>
         /// Image title
         /// </summary>
-        public string title;
+        public string title { get; set; }
         /// <summary>
         /// Image description
         /// </summary>
-        public string description;
+        public string description { get; set; }
         /// <summary>
         /// Mime type
         /// </summary>
-        public string type;
+        public string type { get; set; }
         /// <summary>
         /// Date and time of upload
         /// </summary>
         [JsonConverter(typeof(UnixDateTimeConverter))]
-        public DateTime datetime;
+        public DateTime datetime { get; set; }
         /// <summary>
         /// True, if animated gif or video
         /// </summary>
-        public bool animated;
+        public bool animated { get; set; }
         /// <summary>
         /// Width in pixels
         /// </summary>
-        public int width;
+        public int width { get; set; }
         /// <summary>
         /// Height in pixels
         /// </summary>
-        public int height;
+        public int height { get; set; }
         /// <summary>
         /// Size in bytes
         /// </summary>
-        public int size;
+        public int size { get; set; }
         /// <summary>
         /// Number of views
         /// </summary>
-        public long views;
+        public long views { get; set; }
         /// <summary>
         /// Amount of bandwidth used (in bytes)
         /// </summary>
-        public long bandwidth;
+        public long bandwidth { get; set; }
         /// <summary>
         /// True if in your favorites
         /// </summary>
-        public bool favorite;
+        public bool favorite { get; set; }
         /// <summary>
         /// Account URL of owner
         /// </summary>
-        public string account_url;
+        public string account_url { get; set; }
         /// <summary>
         /// Account ID of owner
         /// </summary>
-        public int? account_id;
+        public int? account_id { get; set; }
         /// <summary>
         /// "True" if this image is an ad
         /// </summary>
-        public bool is_ad;
+        public bool is_ad { get; set; }
         /// <summary>
         /// "True" if currently listed in the most viral section
         /// </summary>
-        public bool in_most_viral;
+        public bool in_most_viral { get; set; }
         /// <summary>
         /// "True" if the video has sound
         /// </summary>
-        public bool has_sound;
+        public bool has_sound { get; set; }
         /// <summary>
         /// Tags associated with the image
         /// </summary>
-        public string[] tags;
+        public string[] tags { get; set; }
         /// <summary>
         /// Ad type
         /// </summary>
-        public int ad_type;
+        public int ad_type { get; set; }
         /// <summary>
         /// Ad URL
         /// </summary>
-        public string ad_url;
+        public string ad_url { get; set; }
         /// <summary>
         /// True if listed in the gallery (front page)
         /// </summary>
-        public bool in_gallery;
+        public bool in_gallery { get; set; }
         /// <summary>
         /// Delete hash for anonymous removal and edit requests
         /// </summary>
-        public string deletehash;
+        public string deletehash { get; set; }
         /// <summary>
         /// Original image file name
         /// </summary>
-        public string name;
+        public string name { get; set; }
         /// <summary>
         /// Image URL
         /// </summary>
-        public string link;
+        public string link { get; set; }
         //unknown data types
-        //public ? vote;
-        //public ? nsfw;
-        //public ? section;
+        //public ? vote { get; set; }
+        //public ? nsfw { get; set; }
+        //public ? section { get; set; }
 
         /// <summary>
         /// Gets the full image URL according to the given size parameter
