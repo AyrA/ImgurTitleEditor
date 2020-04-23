7bbd195c39f7e6c7f9ece818be17240a40711f8a
AyrA <admin@ayra.ch>
2020-04-24 01:54:08 +0200

    Update base response class

--- ImgurTitleEditor/ImgurResponse.cs
+++ ImgurTitleEditor/ImgurResponse.cs
@@ -4,6 +4,7 @@
     /// Generic Imgur API response
     /// </summary>
     /// <typeparam name="T">Type of <see cref="data"/> property</typeparam>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Imgur API")]
     public class ImgurResponse<T>
     {
         /// <summary>
@@ -13,15 +14,15 @@
         /// <summary>
         /// Imgur API status code
         /// </summary>
-        public int status;
+        public int status { get; set; }
         /// <summary>
         /// True if answer is considered successful
         /// </summary>
-        public bool success;
+        public bool success { get; set; }
         /// <summary>
         /// API specific data type
         /// </summary>
-        public T data;
+        public T data { get; set; }
 
         /// <summary>
         /// Initializes a new Imgur response
